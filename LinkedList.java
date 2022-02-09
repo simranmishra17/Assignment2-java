48dd32d3742ca67327e808ce1d0868d39cb4438d
SIMRAN MISHRA <53179520+simranmishra17@users.noreply.github.com>
2022-02-09 23:18:12 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,2 +1,75 @@
 public class LinkedList {
+
+    Node head;
+    static class Node {
+
+        int data;
+        Node next;
+
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static LinkedList insert(LinkedList list, int data)
+    {
+        Node new_node= new Node(data);
+        new_node.next = null;
+
+        if (list.head == null) {
+            list.head = new_node;
+        }
+        else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new_node;
+        }
+        return list;
+    }
+
+    public static void printList(LinkedList list)
+    {
+        Node currNode = list.head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " ");
+            currNode = currNode.next;
+        }
+    }
+    public static Node reverse(Node node)
+    {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
+    }
+
+    public static void main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+        list = insert(list, 1);
+        list = insert(list, 2);
+        list = insert(list, 3);
+        list = insert(list, 4);
+        list = insert(list, 5);
+        
+
+        printList(list);
+        list.head = reverse(list.head);
+        System.out.println(" ");
+        printList(list);
+    }
 }
+
+
+
